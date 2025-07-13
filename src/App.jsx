47d4b68f4312adf3bf9afab6319b80b38e01e25f
abhinavA4 <abhinav.dev@yahoo.com>
2025-07-13 23:45:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-black h-screen overflow-y-auto min-h-[1200px] grid grid-cols-2 grid-rows-25 sm:grid-cols-17 sm:grid-rows-19 gap-3 p-3 text-white">
-      <div className="element row-span-3 row-start-8 col-span-2 sm:col-span-9 sm:row-span-5">1</div>
+      <div className="element row-span-3 row-start-8 col-span-2 sm:col-span-9 sm:row-span-5"></div>
       <div className="element row-span-3 sm:col-span-5 sm:row-span-5">2</div>
       <div className="element row-span-4 col-start-2 row-start-18 sm:row-span-8">3</div>
       <div className="element row-span-3 col-span-2 sm:col-span-5 sm:row-span-5">4</div>
