5798a5f9b7847bc5576d13b067610eddf77e8b4d
abhinavA4 <abhinav.dev@yahoo.com>
2025-07-14 09:53:30 +0530

    layout changes 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="bg-black h-screen overflow-y-auto min-h-[1200px] grid grid-cols-2 grid-rows-25 sm:grid-cols-17 sm:grid-rows-19 gap-3 p-3 text-white">
       <div className="element row-span-3 row-start-8 col-span-2 sm:col-span-9 sm:row-span-5"></div>
       <div className="element row-span-3 sm:col-span-5 sm:row-span-5">2</div>
-      <div className="element row-span-4 col-start-2 row-start-18 sm:row-span-8">3</div>
+      <div className="element row-span-4 col-start-2 row-start-18 sm:row-span-8 sm:col-start-15 sm:col-span-3">3</div>
       <div className="element row-span-3 col-span-2 sm:col-span-5 sm:row-span-5">4</div>
       <div className="element row-span-4 col-start-1 row-start-1 col-span-2 sm:col-span-6 sm:row-span-8">5</div>
       <div className="element row-span-4 sm:col-span-3 sm:row-span-8">6</div>
